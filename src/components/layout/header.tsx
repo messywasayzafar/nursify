f6cdbc12dbcac2ce35a26f4db2491c6ebb076822
Hexagon Enterprises <hexagonenterprises.me@gmail.com>
2025-09-01 10:19:27 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="relati

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,7 +3,6 @@ import {
   CircleUser,
   Menu,
   Package2,
-  Search,
   Stethoscope,
   Bell
 } from 'lucide-react';
@@ -63,16 +62,7 @@ export function AppHeader() {
       </Sheet>
 
       <div className="w-full flex-1">
-        <form>
-          <div className="relative">
-            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-            <Input
-              type="search"
-              placeholder="Search patients, staff..."
-              className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
-            />
-          </div>
-        </form>
+        {/* Search bar removed */}
       </div>
 
       <Button variant="ghost" size="icon" className="rounded-full">
