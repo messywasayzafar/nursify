3fb02e95671332430159314565df2a55f5e53247
Hexagon Enterprises <hexagonenterprises.me@gmail.com>
2025-09-01 16:21:57 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <X className="h-4

--- src/components/report/report-center-modal.tsx
+++ src/components/report/report-center-modal.tsx
@@ -6,10 +6,8 @@ import {
   DialogContent,
   DialogHeader,
   DialogTitle,
-  DialogClose,
 } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
-import { X } from 'lucide-react';
 
 interface ReportCenterModalProps {
   setOpen: (open: boolean) => void;
@@ -27,12 +25,6 @@ export function ReportCenterModal({ setOpen }: ReportCenterModalProps) {
     <DialogContent className="sm:max-w-sm p-0">
       <DialogHeader className="p-4 bg-primary text-primary-foreground">
         <DialogTitle className="text-center text-xl">Report Center</DialogTitle>
-        <DialogClose asChild>
-          <button className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-primary data-[state=open]:text-primary-foreground">
-            <X className="h-4 w-4" />
-            <span className="sr-only">Close</span>
-          </button>
-        </DialogClose>
       </DialogHeader>
       <div className="grid gap-4 p-6">
         {reportButtons.map((buttonLabel) => (
